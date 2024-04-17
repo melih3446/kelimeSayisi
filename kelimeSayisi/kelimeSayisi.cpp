13ace41f6791e874ae15a47db6efda43c043bdf8
mgozu <mgozu@DESKTOP-J2K9HDU>
2024-04-17 19:34:03 +0300

    Proje dosyası ekle.

---
+++ kelimeSayisi/kelimeSayisi.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string kelime;
+    int kelimeHarfSayisi = 0;
+
+
+
+    cout << "bir kelime giriniz:";
+    cin >> kelime;
+
+    while (kelime[kelimeHarfSayisi] != '\0')
+    {
+        kelimeHarfSayisi++;
+    }
+    cout << "girdiginiz kelime: " << kelimeHarfSayisi << " karakterlidir";
+}
